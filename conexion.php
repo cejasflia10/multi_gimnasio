d7e41674e663bc6f6468d98b29d1b06c2d6fd925
cejasflia10 <cejasdan24533@gmail.com>
2025-06-18 14:42:33 +0200

    main

--- conexion.php
+++ conexion.php
@@ -1,5 +1,19 @@
 <?php
-$conexion = new mysqli("localhost", "usuario", "clave", "multi_gimnasio");
+// Evitar errores por salida previa
+ob_start();
+
+// Mostrar errores en pantalla
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$host = 'shuttle.proxy.rlwy.net';
+$puerto = 51676;
+$usuario = 'root';
+$contrasena = 'bZwtwptDJTaiWydjpfMWTBGwcwMzSKTt';
+$basedatos = 'railway';
+
+$conexion = new mysqli($host, $usuario, $contrasena, $basedatos, $puerto);
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
