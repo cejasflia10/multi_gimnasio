f584eb3a01585af5b323d3dabdb51238e124d76c
cejasflia10 <cejasdan24533@gmail.com>
2025-06-18 14:49:38 +0200

    main

--- conexion.php
+++ conexion.php
@@ -1,20 +1,20 @@
 <?php
-// Evitar errores por salida previa
+// Mostrar errores en pantalla (para debug)
 ob_start();
-
-// Mostrar errores en pantalla
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 $host = 'shuttle.proxy.rlwy.net';
 $puerto = 51676;
 $usuario = 'root';
-$contrasena = 'bZwtwptDJTaiWydjpfMWTBGwcwMzSKTt';
+$contrasena = 'bZwtwptDJTailwydjpfMWTBGwcwMzSKTt';
 $basedatos = 'railway';
 
 $conexion = new mysqli($host, $usuario, $contrasena, $basedatos, $puerto);
 if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
+    die("❌ Error de conexión: " . $conexion->connect_error);
+    exit;
 }
 ?>
