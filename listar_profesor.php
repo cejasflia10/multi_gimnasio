9ea8050498209715fc2015b998f8c797dad2a649
cejasflia10 <cejasdan24533@gmail.com>
2025-06-21 14:39:46 +0200

    main

--- listar_profesor.php
+++ listar_profesor.php
@@ -1,6 +1,7 @@
-
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 include 'conexion.php';
 if (!isset($_SESSION['gimnasio_id'])) {
     die("Acceso denegado.");
