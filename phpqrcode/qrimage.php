c3dc739b20ee060e7fdf216f3fef5553fca2bd13
cejasflia10 <cejasdan24533@gmail.com>
2025-06-22 23:06:12 +0200

    main

--- phpqrcode/qrimage.php
+++ phpqrcode/qrimage.php
@@ -1,88 +1,45 @@
-<?php
-/*cambio forzado para que git detecte el cambio
- * PHP QR Code encoder
- *
- * Image output of code using GD2
- */
+private static function image($frame, $pixelPerPoint = 4, $outerFrame = 4, $back_color = 0xFFFFFF, $fore_color = 0x000000)
+{
+    // Conversión segura a int
+    $pixelPerPoint = intval($pixelPerPoint);
+    $outerFrame = intval($outerFrame);
+    $back_color = intval($back_color);
+    $fore_color = intval($fore_color);
 
-define('QR_IMAGE', true);
+    $h = count($frame);
+    $w = strlen($frame[0]);
 
-class QRimage {
+    $imgW = $w + 2 * $outerFrame;
+    $imgH = $h + 2 * $outerFrame;
 
-    //----------------------------------------------------------------------  
-    public static function png($frame, $back_color, $fore_color, $filename = false, $pixelPerPoint = 4, $outerFrame = 4, $saveandprint = FALSE)
-    {
-        $image = self::image($frame, $pixelPerPoint, $outerFrame, $back_color, $fore_color);
+    $base_image = ImageCreate($imgW, $imgH);
 
-        if ($filename === false) {
-            Header("Content-type: image/png");
-            ImagePng($image);
-        } else {
-            if ($saveandprint === TRUE) {
-                ImagePng($image, $filename);
-                header("Content-type: image/png");
-                ImagePng($image);
-            } else {
-                ImagePng($image, $filename);
+    // Foreground color
+    $r1 = ($fore_color & 0xFF0000) >> 16;
+    $g1 = ($fore_color & 0x00FF00) >> 8;
+    $b1 = ($fore_color & 0x0000FF);
+
+    // Background color
+    $r2 = ($back_color & 0xFF0000) >> 16;
+    $g2 = ($back_color & 0x00FF00) >> 8;
+    $b2 = ($back_color & 0x0000FF);
+
+    $col[0] = ImageColorAllocate($base_image, $r2, $g2, $b2);
+    $col[1] = ImageColorAllocate($base_image, $r1, $g1, $b1);
+
+    imagefill($base_image, 0, 0, $col[0]);
+
+    for ($y = 0; $y < $h; $y++) {
+        for ($x = 0; $x < $w; $x++) {
+            if ($frame[$y][$x] == '1') {
+                ImageSetPixel($base_image, $x + $outerFrame, $y + $outerFrame, $col[1]);
             }
         }
-
-        ImageDestroy($image);
     }
 
-    //----------------------------------------------------------------------
-    public static function jpg($frame, $filename = false, $pixelPerPoint = 8, $outerFrame = 4, $q = 85)
-    {
-        $image = self::image($frame, $pixelPerPoint, $outerFrame);
+    $target_image = ImageCreate($imgW * $pixelPerPoint, $imgH * $pixelPerPoint);
+    ImageCopyResized($target_image, $base_image, 0, 0, 0, 0, $imgW * $pixelPerPoint, $imgH * $pixelPerPoint, $imgW, $imgH);
+    ImageDestroy($base_image);
 
-        if ($filename === false) {
-            Header("Content-type: image/jpeg");
-            ImageJpeg($image, null, $q);
-        } else {
-            ImageJpeg($image, $filename, $q);
-        }
-
-        ImageDestroy($image);
-    }
-
-    //----------------------------------------------------------------------
-    private static function image($frame, $pixelPerPoint = 4, $outerFrame = 4, $back_color = 0xFFFFFF, $fore_color = 0x000000)
-    {
-        $h = count($frame);
-        $w = strlen($frame[0]);
-
-        $imgW = $w + 2 * $outerFrame;
-        $imgH = $h + 2 * $outerFrame;
-
-        $base_image = ImageCreate($imgW, $imgH);
-
-        // Foreground color
-        $r1 = round((($fore_color & 0xFF0000) >> 16), 5);
-        $g1 = round((($fore_color & 0x00FF00) >> 8), 5);
-        $b1 = round(($fore_color & 0x0000FF), 5);
-
-        // Background color
-        $r2 = round((($back_color & 0xFF0000) >> 16), 5);
-        $g2 = round((($back_color & 0x00FF00) >> 8), 5);
-        $b2 = round(($back_color & 0x0000FF), 5);
-
-        $col[0] = ImageColorAllocate($base_image, $r2, $g2, $b2);
-        $col[1] = ImageColorAllocate($base_image, $r1, $g1, $b1);
-
-        imagefill($base_image, 0, 0, $col[0]);
-
-        for ($y = 0; $y < $h; $y++) {
-            for ($x = 0; $x < $w; $x++) {
-                if ($frame[$y][$x] == '1') {
-                    ImageSetPixel($base_image, $x + $outerFrame, $y + $outerFrame, $col[1]);
-                }
-            }
-        }
-
-        $target_image = ImageCreate($imgW * $pixelPerPoint, $imgH * $pixelPerPoint);
-        ImageCopyResized($target_image, $base_image, 0, 0, 0, 0, $imgW * $pixelPerPoint, $imgH * $pixelPerPoint, $imgW, $imgH);
-        ImageDestroy($base_image);
-
-        return $target_image;
-    }
+    return $target_image;
 }
