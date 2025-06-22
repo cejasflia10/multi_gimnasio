11059c5967c5cbe392ca09ecf2e55d1ec4c44117
cejasflia10 <cejasdan24533@gmail.com>
2025-06-22 03:06:21 +0200

    main

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 include 'menu.php';
 include 'conexion.php';
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 if (!isset($_SESSION['gimnasio_id'])) {
     die("Acceso denegado.");
