071700ebf5af9a625fdc82c32037fac4c1c3317f
cejasflia10 <cejasdan24533@gmail.com>
2025-06-24 03:55:29 +0200

    main

--- obtener_datos_plan.php
+++ obtener_datos_plan.php
@@ -1,16 +1,17 @@
-
 <?php
-include("conexion.php");
+include 'conexion.php';
 
 if (isset($_GET['plan_id'])) {
     $plan_id = intval($_GET['plan_id']);
+
     $query = "SELECT precio, duracion, clases_disponibles FROM planes WHERE id = $plan_id";
     $resultado = $conexion->query($query);
+
     if ($fila = $resultado->fetch_assoc()) {
         echo json_encode([
             'precio' => $fila['precio'],
             'duracion' => $fila['duracion'],
-            'clases_disponibles' => $fila['clases_disponibles']
+            'disponibles' => $fila['clases_disponibles']
         ]);
     } else {
         echo json_encode(['error' => 'Plan no encontrado']);
