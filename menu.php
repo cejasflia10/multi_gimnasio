d14f56cc348a67a3b674a58a6d78adf637f6b185
cejasflia10 <cejasdan24533@gmail.com>
2025-06-25 01:37:10 +0200

    main

--- menu.php
+++ menu.php
@@ -84,7 +84,6 @@ body {
     </div>
 <?php endif; ?>
 
-
 <!-- BOTÓN ☰ PARA CELULARES -->
 <button class="menu-toggle" onclick="toggleMenu()">☰</button>
 
@@ -119,14 +118,6 @@ body {
         <a href="ver_asistencia.php">Ver Asistencias</a>
         <a href="registro_online.php" target="_blank"><i class="fas fa-link"></i> Registro Online</a>
     </div>
-     <?php if ($rol === 'cliente'): ?>
-     <div class="submenu-toggle" onclick="toggleSubmenu('seguimientoClienteSubmenu')"><i class="fas fa-apple-alt"></i> Nutrición</div>
-     <div class="submenu" id="seguimientoClienteSubmenu">
-        <a href="ficha_habitos.php"><i class="fas fa-edit"></i> Ficha de Hábitos</a>
-        <a href="ver_habitos_profesor.php?id=<?= $_SESSION['cliente_id'] ?? 0 ?>"><i class="fas fa-eye"></i> Ver Ficha</a>
-        <a href="ver_seguimiento_cliente.php"><i class="fas fa-list"></i> Seguimiento Semanal</a>
-     </div>
-<?php endif; ?>
 
     <div class="submenu-toggle" onclick="toggleSubmenu('qrSubmenu')"><i class="fas fa-qrcode"></i> QR</div>
     <div class="submenu" id="qrSubmenu">
@@ -156,6 +147,15 @@ body {
         <a href="ver_turnos_cliente.php"><i class="fas fa-calendar-alt"></i> Ver Mis Turnos</a>
         <a href="estado_pagos.php"><i class="fas fa-dollar-sign"></i> Estado de Pagos</a>
         <a href="mi_qr.php"><i class="fas fa-qrcode"></i> Mi Código QR</a>
+    </div>
+    <?php endif; ?>
+
+    <?php if ($rol === 'cliente'): ?>
+    <div class="submenu-toggle" onclick="toggleSubmenu('seguimientoClienteSubmenu')"><i class="fas fa-apple-alt"></i> Nutrición</div>
+    <div class="submenu" id="seguimientoClienteSubmenu">
+        <a href="ficha_habitos.php"><i class="fas fa-edit"></i> Ficha de Hábitos</a>
+        <a href="ver_habitos_profesor.php?id=<?= $_SESSION['cliente_id'] ?? 0 ?>"><i class="fas fa-eye"></i> Ver Ficha</a>
+        <a href="ver_seguimiento_cliente.php"><i class="fas fa-list"></i> Seguimiento Semanal</a>
     </div>
     <?php endif; ?>
 
