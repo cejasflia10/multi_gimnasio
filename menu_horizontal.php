c9d1446faa0b0c571c4125bb51afaa17c1290653
cejasflia10 <cejasdan24533@gmail.com>
2025-06-26 21:11:01 +0200

    main

--- menu_horizontal.php
+++ menu_horizontal.php
@@ -98,13 +98,18 @@
             <a href="generar_qr.php">Generar</a>
         </div>
     </div>
-    <div class="dropdown">
-        <span class="dropbtn"><i class="fas fa-user-tie"></i> Profesores</span>
-        <div class="dropdown-content">
-            <a href="agregar_profesor.php">Agregar</a>
-            <a href="ver_profesores.php">Ver</a>
-            <a href="ver_pagos_profesor.php">Pagos</a>
-        </div>
+    <?php if (tiene_permiso('profesores')): ?>
+<li class="submenu">
+    <a href="#">👨‍🏫 Profesores</a>
+    <ul>
+        <li><a href="agregar_profesor.php">➕ Agregar Profesor</a></li>
+        <li><a href="ver_profesores.php">📋 Ver Profesores</a></li>
+        <li><a href="plan_profesor.php">💵 Plan por Hora</a></li>
+        <li><a href="ver_pagos_profesor.php">💰 Ver Pagos</a></li>
+    </ul>
+</li>
+<?php endif; ?>
+
     </div>
     <div class="dropdown">
         <span class="dropbtn"><i class="fas fa-shopping-cart"></i> Ventas</span>
