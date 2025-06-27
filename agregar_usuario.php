ebe954c84aa0e20e600724a384f132d158d45952
cejasflia10 <cejasdan24533@gmail.com>
2025-06-27 03:36:27 +0200

    main

--- agregar_usuario.php
+++ agregar_usuario.php
@@ -4,7 +4,6 @@ if (!isset($_SESSION['rol']) || $_SESSION['rol'] !== 'admin') {
     die("Acceso denegado.");
 }
 include 'conexion.php';
-include 'menu_horizontal.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $usuario = trim($_POST["usuario"]);
