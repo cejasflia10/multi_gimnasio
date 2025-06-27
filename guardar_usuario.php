1a51f3fa10cbbe0cabe871e908cb07825d3a20de
cejasflia10 <cejasdan24533@gmail.com>
2025-06-27 13:31:05 +0200

    main

--- guardar_usuario.php
+++ guardar_usuario.php
@@ -11,8 +11,23 @@ if (
     $rol = $conexion->real_escape_string($_POST['rol']);
     $gimnasio_id = intval($_POST['gimnasio_id']);
 
+    // Permisos
+    $permisos = isset($_POST['permisos']) ? implode(',', $_POST['permisos']) : '';
+
+    // Si se carga nueva contraseña
+    $update_password = "";
+    if (!empty($_POST['nueva_contrasena'])) {
+        $password = $conexion->real_escape_string($_POST['nueva_contrasena']);
+        $hashed = password_hash($password, PASSWORD_DEFAULT);
+        $update_password = ", contrasena = '$hashed'";
+    }
+
     $query = "UPDATE usuarios 
-              SET nombre_usuario = '$nombre_usuario', rol = '$rol', gimnasio_id = $gimnasio_id 
+              SET nombre_usuario = '$nombre_usuario',
+                  rol = '$rol',
+                  gimnasio_id = $gimnasio_id,
+                  permisos = '$permisos'
+                  $update_password
               WHERE id = $id";
 
     if ($conexion->query($query)) {
