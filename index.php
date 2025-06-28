1731b47ebc3161f4ce611dab23e715139d786e9d
cejasflia10 <cejasdan24533@gmail.com>
2025-06-28 22:31:19 +0200

    main

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 include 'conexion.php';
+include 'menu_horizontal.php';
 
 $gimnasio_id = $_SESSION['gimnasio_id'] ?? 0;
 
@@ -128,53 +129,6 @@ if ($deudas_q && $deudas_q->num_rows > 0) {
     <?= $cliente_activo ?>
   </div>
 </header>
-
-<nav>
-  
-  <div class="dropdown"><span class="dropbtn"><i class="fas fa-users"></i> Clientes</span>
-    <div class="dropdown-content">
-      <a href="agregar_cliente.php">Agregar</a>
-      <a href="ver_clientes.php">Ver</a>
-      <a href="disciplinas.php">Disciplinas</a>
-    </div>
-  </div>
-
-  <div class="dropdown"><span class="dropbtn"><i class="fas fa-id-card"></i> Membresías</span>
-    <div class="dropdown-content"><a href="nueva_membresia.php">Nueva</a><a href="ver_membresias.php">Ver</a><a href="planes.php">Planes</a><a href="adicionales.php">Adicionales</a></div>
-  </div>
-  <div class="dropdown"><span class="dropbtn"><i class="fas fa-calendar-check"></i> Asistencias</span>
-    <div class="dropdown-content"><a href="registrar_asistencia.php">Registrar</a><a href="ver_asistencias.php">Ver</a></div>
-  </div>
-  <div class="dropdown"><span class="dropbtn"><i class="fas fa-qrcode"></i> QR</span>
-    <div class="dropdown-content"><a href="scanner_qr.php">Escanear</a><a href="generar_qr.php">Generar</a></div>
-  </div>
-  <div class="dropdown"><span class="dropbtn"><i class="fas fa-user-tie"></i> Profesores</span>
-    <div class="dropdown-content"><a href="agregar_profesor.php">Agregar</a><a href="ver_profesores.php">Ver</a><a href="ver_pagos_profesor.php">Pagos</a></div>
-  </div>
-  <div class="dropdown"><span class="dropbtn"><i class="fas fa-shopping-cart"></i> Ventas</span>
-    <div class="dropdown-content"><a href="ventas_protecciones.php">Protecciones</a><a href="ventas_indumentaria.php">Indumentaria</a><a href="ventas_suplementos.php">Suplementos</a><a href="ver_ventas.php">Ver Todas</a></div>
-  </div>
-  <div class="dropdown"><span class="dropbtn"><i class="fas fa-dumbbell"></i> Gimnasios</span>
-    <div class="dropdown-content"><a href="agregar_gimnasio.php">Agregar</a><a href="ver_gimnasios.php">Ver</a></div>
-  </div>
-  <div class="dropdown"><span class="dropbtn"><i class="fas fa-user-cog"></i> Usuarios</span>
-    <div class="dropdown-content"><a href="agregar_usuario.php">Agregar</a><a href="ver_usuarios.php">Ver</a></div>
-  </div>
-  <div class="dropdown"><span class="dropbtn"><i class="fas fa-cogs"></i> Configuraciones</span>
-    <div class="dropdown-content"><a href="configurar_planes.php">Planes</a><a href="configurar_accesos.php">Accesos</a></div>
-  </div>
-    <div class="dropdown"><span class="dropbtn">Panel del Cliente</span>
-    <div class="dropdown-content">
-      <a href="cliente_acceso.php">Acceso DNI</a>
-      <a href="cliente_reservas.php">Reservas</a>
-      <a href="cliente_pagos.php">Pagos</a>
-      <a href="cliente_asistencias.php"><i class="fas fa-calendar-check"></i> Asistencias</a>
-      <a href="cliente_qr.php">Ver QR</a>
-    </div>
-  </div>
-  <a href="logout.php" class="dropbtn"><i class="fas fa-sign-out-alt"></i> Cerrar Sesión</a>
-
-</nav>
 
 <div class="container">
   <div class="panel">
