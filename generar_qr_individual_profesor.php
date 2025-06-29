40cb6a1250812e5aade0bd636723d554e77560dd
cejasflia10 <cejasdan24533@gmail.com>
2025-06-29 20:26:54 +0200

    main

--- generar_qr_individual_profesor.php
+++ generar_qr_individual_profesor.php
@@ -3,6 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+echo "Iniciando generación QR...<br>";
+
 include 'conexion.php';
 require_once 'phpqrcode/qrlib.php';
 session_start();
@@ -12,6 +14,8 @@ if (!isset($_GET['id'])) {
 }
 
 $id = intval($_GET['id']);
+echo "ID recibido: $id<br>";
+
 $profesor_q = $conexion->query("SELECT dni FROM profesores WHERE id = $id");
 if ($profesor_q->num_rows === 0) {
     die("Profesor no encontrado.");
@@ -19,11 +23,13 @@ if ($profesor_q->num_rows === 0) {
 
 $profesor = $profesor_q->fetch_assoc();
 $dni = $profesor['dni'];
-$qr_code = 'P-' . $dni;
+echo "DNI: $dni<br>";
 
+$qr_code = 'P-' . $dni;
 $filename = __DIR__ . "/qr/qr_profesor_P-$dni.png";
+
+echo "Generando archivo en: $filename<br>";
 QRcode::png($qr_code, $filename, 'H', 10);
 
-header("Location: ver_profesores.php");
-exit;
+echo "QR generado correctamente.<br>";
 ?>
