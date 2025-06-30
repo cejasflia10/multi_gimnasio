6c678f92e5329cfaf01ba6a46a6938b0ed969df1
cejasflia10 <cejasdan24533@gmail.com>
2025-06-30 04:39:14 +0200

    main

--- generar_qr_profesor.php
+++ generar_qr_profesor.php
@@ -7,8 +7,9 @@ if (!isset($_GET['dni'])) {
 }
 
 $dni = intval($_GET['dni']);
-$codigo = "P" . $dni;
+$codigo = "P$dni"; // SIN GUION
 
+// Generar QR en tiempo real (Render no permite guardar archivo)
 header('Content-Type: image/png');
 QRcode::png($codigo, false, QR_ECLEVEL_L, 8);
 ?>
