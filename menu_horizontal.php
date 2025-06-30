8bca54d3aa4a50bbf230808327b0aba65776b480
cejasflia10 <cejasdan24533@gmail.com>
2025-06-30 17:50:04 +0200

    main

--- menu_horizontal.php
+++ menu_horizontal.php
@@ -1,4 +1,5 @@
-<?php if (session_status() === PHP_SESSION_NONE) { session_start(); } ?>
+<?php 
+if (session_status() === PHP_SESSION_NONE) { session_start(); } ?>
 
 <!DOCTYPE html>
 <html lang="es">
