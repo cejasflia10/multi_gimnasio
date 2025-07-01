6af2cf0aad6da544ca892b786337c591bb7fa92a
cejasflia10 <cejasdan24533@gmail.com>
2025-07-01 20:24:08 +0200

    main

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,14 @@
-
 self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('multi-gimnasio').then(function(cache) {
-      return cache.addAll([
-        // Cliente
-        'login_cliente.php',
-        'panel_cliente.php',
-        'manifest_cliente.json',
-        'icono_cliente.png',
-
-        // Profesor
-        'login_profesor.php',
-        'panel_profesor.php',
-        'manifest_profesor.json',
-        'icono_profesor.png'
-      ]);
-    })
-  );
+  console.log('SW instalado');
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('activate', function(e) {
+  console.log('SW activado');
+  e.waitUntil(clients.claim());
+});
+
+// No guardar en cache
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
 });
