2c9df1e1f2c4308146e2b17c41db1033992ec6bc
cejasflia10 <cejasdan24533@gmail.com>
2025-07-02 16:37:32 +0200

    main

--- agregar_producto.php
+++ agregar_producto.php
@@ -1,6 +1,7 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) session_start();
 include 'conexion.php';
+include 'menu_horizontal.php';
 
 $mensaje = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
