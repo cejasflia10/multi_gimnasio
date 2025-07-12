132d6d73ce2c9617ecdf45606ebe6c9cfaa480d9
cejasflia10 <cejasdan24533@gmail.com>
2025-07-12 15:40:13 -0300

    main

--- conexion.php
+++ conexion.php
@@ -1,16 +1,14 @@
 <?php
-// Evitar errores por salida previa
-ob_start();
-
-// Mostrar errores en pantalla
+// Mostrar errores
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// Datos reales desde Railway (verificados con tus capturas)
 $host = 'shuttle.proxy.rlwy.net';
 $puerto = 51676;
 $usuario = 'root';
-$contrasena = 'bzkntwpED3TaihydJpFM7B6wcWtSKTf3'; // CONTRASEÑA CORRECTA de Railway
+$contrasena = 'bzkntwpED3TaihydJpFM7B6wcWtSKTf3'; // Copialo exactamente desde Railway
 $basedatos = 'railway';
 
 $conexion = new mysqli($host, $usuario, $contrasena, $basedatos, $puerto);
